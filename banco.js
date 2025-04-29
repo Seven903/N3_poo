42e780f34d170c85e3d689631ca419601515992e
Daniel Alves <danielallves903@gmail.com>
2025-04-29 10:56:58 -0300

    opa

--- banco.js
+++ banco.js
@@ -3,22 +3,22 @@ const db = new sqlite3.Database("banco.db");
 
 db.serialize(() => {
   db.run(`
-    CREATE TABLE musicas(
-      id_musica INTEGER PRIMARY KEY, 
+    CREATE TABLE IF NOT EXISTS musicas(
+      id_musica INTEGER PRIMARY KEY AUTOINCREMENT, 
       nome TEXT NOT NULL, 
       genero TEXT NOT NULL
     );
   `);
 
   db.run(`
-    CREATE TABLE artistas(
-      id_artista INTEGER PRIMARY KEY,
-      nome TEXT NOT NULL,
+    CREATE TABLE IF NOT EXISTS artistas(
+      id_artista INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL
     );
   `);
 
   db.run(`
-    CREATE TABLE artista_musica(
+    CREATE TABLE IF NOT EXISTS artista_musica(
       id_artista INTEGER,
       id_musica INTEGER,
       FOREIGN KEY (id_artista) REFERENCES artistas (id_artista),
@@ -26,20 +26,20 @@ db.serialize(() => {
     );
   `);
   db.run(`
-    CREATE TABLE programa(
-      id_programa INTEGER PRIMARY KEY,
-      nome TEXT NOT NULL,
+    CREATE TABLE IF NOT EXISTS programa(
+      id_programa INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL
     );
   `);
   db.run(`
-    CREATE TABLE ouvinte(
+    CREATE TABLE IF NOT EXISTS ouvinte(
       celular INTEGER PRIMARY KEY,
       nome INTEGER,
-      bairro TEXT NOT NULL,
+      bairro TEXT NOT NULL
     );
   `);
   db.run(`
-    CREATE TABLE pedido(
+    CREATE TABLE IF NOT EXISTS pedido(
       dt_hr INTEGER PRIMARY KEY,
       id_musica INTEGER,
       id_programa INTEGER,
@@ -50,3 +50,5 @@ db.serialize(() => {
   `);
 });
 db.close();
+
+module.exports = db
--- radio.js
+++ radio.js
@@ -1,4 +1,45 @@
+const db = require("./banco");
+const prompt = require("prompt-sync")();
 
-const db = require("./banco.js");
+class Musicas {
+  constructor(id = null, nome, genero) {
+    this.id = id;
+    this.nome = nome;
+    this.genero = genero;
+  }
+}
 
+class Artistas {
+  constructor(id = null, nome) {
+    this.id = id;
+    this.nome = nome;
+  }
 
+  salvar() {
+    const smt = db.prepare(`INSERT INTO artistas (nome) VALUES (?)`);
+    for (let i = 1; i <= 5; i++) {
+    
+    }
+    smt.finalize();
+  }
+}
+
+class Programa {
+  constructor(id = null, nome) {
+    this.id = id;
+    this.nome = nome;
+  }
+}
+
+class Pedido {
+  constructor(horario, id_musica, id_progama, id_ouvinte) {
+    this.horario = horario;
+    this.id_musica = id_musica;
+    this.id_progama = id_progama;
+    this.id_ouvinte = id_ouvinte;
+  }
+}
+
+let a1 = new Artistas(null, "roberto carlos");
+
+a1;
